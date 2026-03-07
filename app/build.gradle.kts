e927a071fc00bcb6b9fb0183f082f33ef41982ed
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-03-07 16:26:03 +0100

    Canvas: Rework dependencies
    - Update Gradle wrapper and AGP to the latest version
    - Update Kotlin to v2.2.10
    - Update generatebp to v1.31
    - Match dependencies version to AOSP prebuilts
    - Remove Compose BOM, manually manage the version
    - Run generatebp
    
    Change-Id: Iaaf7e68cf7525a50dc1a7960b63522e536d6434f

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,27 +51,25 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
-
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
-
     implementation(libs.androidx.compose.ui.tooling.preview)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.coil.compose)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
 
 generateBp {
     targetSdk = android.defaultConfig.targetSdk!!
     minSdk = android.defaultConfig.minSdk!!
+    versionCode = android.defaultConfig.versionCode!!
+    versionName = android.defaultConfig.versionName!!
     availableInAOSP = { module ->
         when {
             module.group.startsWith("androidx") -> true
