9ba4657aed6f6c2d033a24ec3a41ba317711221f
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-03-08 18:21:36 +0100

    Canvas: Always use dark theme
    Google Photos does so as well
    
    Change-Id: Ia30b38f9b84da080a4cbc145df65c1412f6e6266

--- app/src/main/java/org/lineageos/canvas/ui/theme/Theme.kt
+++ app/src/main/java/org/lineageos/canvas/ui/theme/Theme.kt
@@ -5,21 +5,17 @@
 
 package org.lineageos.canvas.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun CanvasTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
     val context = LocalContext.current
-    val colorScheme =
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    val colorScheme = dynamicDarkColorScheme(context)
 
     MaterialTheme(
         colorScheme = colorScheme,
